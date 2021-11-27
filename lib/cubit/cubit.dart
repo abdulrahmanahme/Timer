c9a57335f2c69f5b65232ebaa6702bed123b08e5
abdulrahmam <47110411+abdulrahmam@users.noreply.github.com>
2021-11-27 18:04:47 +0200

    1-Add BLOC to project, 2-CREATE CUBIT file , add to it 3 files , - Add BlocObserver file  , -Add cubit file  , - Add states file      ,

---
+++ lib/cubit/cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:timer/Cubit/states.dart';
+
+class TimerCubit extends Cubit<TimerStates>{
+  TimerCubit(TimerStates initialState) : super(initialState);
+  
+}
+
